68c665c4db7f984c2dfd0b853426dca1895d1dfb
touyou <fujiyou1121@gmail.com>
2019-06-04 10:47:36 +0900

    Initial Commit

---
+++ WWDC2019Sandbox/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WWDC2019Sandbox
+//
+//  Created by 藤井陽介 on 2019/06/04.
+//  Copyright © 2019 touyou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
